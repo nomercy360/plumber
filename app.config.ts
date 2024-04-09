33fc9ec9e66da314bb0909fbed3bcbaa496d4108
maksimkadocnikov <maxim.kadocnikov@scorum.com>
2024-04-09 21:07:52 +0700

    prerender routes

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from '@solidjs/start/config';
 export default defineConfig({
   server: {
     preset: 'cloudflare-pages',
+    prerender: {
+      crawlLinks: true,
+    },
     rollupConfig: {
       external: ['__STATIC_CONTENT_MANIFEST', 'node:async_hooks'],
     },
