5fb1e557efff2324dddaa804d031bb4daa79d7ab
maksimkadocnikov <maxim.kadocnikov@scorum.com>
2024-04-13 21:43:33 +0700

    test suspense

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@solidjs/start/config';
 
 export default defineConfig({
+  ssr: true,
   server: {
     preset: 'cloudflare-pages',
     prerender: {
--- src/components/ProductRecommendations.tsx
+++ src/components/ProductRecommendations.tsx
@@ -1,32 +1,56 @@
-import { For } from 'solid-js';
+import { For, Suspense } from 'solid-js';
+import { cache, createAsync } from '@solidjs/router';
+import { products } from '~/lib/api';
+
+const getProducts = cache(async () => {
+  'use server';
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return products.slice(0, 4);
+}, 'students');
 
 export default function ProductRecommendations(props: { products: any[] }) {
+  const products = createAsync(() => getProducts());
+
   return (
-    <section class='flex w-full flex-col items-center justify-center sm:max-w-[1440px]'>
-      <p class='mb-6 text-sm uppercase sm:mb-14 sm:text-base'>
-        You may also like
-      </p>
-      <div class='flex w-screen flex-row gap-10 overflow-x-auto p-5 sm:grid sm:w-full sm:grid-cols-4 sm:overflow-x-hidden sm:p-14'>
-        <For each={props.products}>
-          {(product) => (
-            <a
-              class='flex min-w-[170px] flex-col items-start justify-start'
-              href={`/product/${product.handle}`}>
-              <img
-                alt=''
-                class='w-full rounded-lg object-cover'
-                src={product.image}
-              />
-              <p class='mb-1 mt-2.5 text-sm sm:mt-4 sm:text-base'>
-                {product.name}
-              </p>
-              <p class='text-xs text-gray-light sm:text-base'>
-                ${product.price}
-              </p>
-            </a>
-          )}
-        </For>
-      </div>
-    </section>
+    <Suspense fallback={<LoaderSkeleton />}>
+      <section class='flex w-full flex-col items-center justify-center sm:max-w-[1440px]'>
+        <p class='mb-6 text-sm uppercase sm:mb-14 sm:text-base'>
+          You may also like
+        </p>
+        <div class='flex w-screen flex-row gap-10 overflow-x-auto p-5 sm:grid sm:w-full sm:grid-cols-4 sm:overflow-x-hidden sm:p-14'>
+          <For each={products()}>
+            {(product) => (
+              <a
+                class='flex min-w-[170px] flex-col items-start justify-start'
+                href={`/product/${product.handle}`}>
+                <img
+                  alt=''
+                  class='w-full rounded-lg object-cover'
+                  src={product.image}
+                />
+                <p class='mb-1 mt-2.5 text-sm sm:mt-4 sm:text-base'>
+                  {product.name}
+                </p>
+                <p class='text-xs text-gray-light sm:text-base'>
+                  ${product.price}
+                </p>
+              </a>
+            )}
+          </For>
+        </div>
+      </section>
+    </Suspense>
   );
 }
+
+const LoaderSkeleton = () => (
+  <div class='flex w-screen flex-row gap-10 overflow-x-auto p-5 sm:grid sm:w-full sm:grid-cols-4 sm:overflow-x-hidden sm:p-14'>
+    <For each={[1, 2, 3, 4]}>
+      {(_, i) => (
+        <div class='flex min-w-[170px] flex-col items-start justify-start'>
+          <div class='h-[420px] w-full animate-pulse rounded-lg bg-gray' />
+        </div>
+      )}
+    </For>
+  </div>
+);
